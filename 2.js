4705c14ceb242ffee8b316f50d040cffe8ae995b
unknown <G:\BAT>
2020-10-31 00:03:06 +0300

    Правка именования переменных

--- 2.js
+++ 2.js
@@ -8,7 +8,7 @@
 Здесь вам пригодятся знания из предыдущего модуля — циклы и условные операторы.
 */
 
-var count = +prompt('Введите простое число от 1 до 1000:', '');
+let count = +prompt('Введите простое число от 1 до 1000:', '');
 
 function isNatural(count) {
     if (count > 1000 || count < 1) {
@@ -17,7 +17,7 @@ function isNatural(count) {
     if (count == 1) {
         return console.log('Число 1 не является простым числом');
     }
-    for (var i = 2; i < count; i++) {
+    for (let i = 2; i < count; i++) {
         if (count % i == 0) {
             return console.log('Вы ввели не простое число!');
             break;
